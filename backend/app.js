bbc97be9e9fb3fae5c2a80a5c900bb57fa9eff3c
Flaviota <flaviota.dev@gmail.com>
2023-09-15 19:58:51 -0300

    Feat: esqueleto das rotas

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const { db } = require('./db/db');
+const {readdirSync} = require('fs')
 const app = express()
 
 require('dotenv').config()
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(cors())
 
 //routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 
 
